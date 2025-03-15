6f67738f8328d3ddb724ddbc8652472100c373cb
GURUGASTGAR <gurugastgar@gmail.com>
2025-03-15 22:21:18 +0530

    character hashing

--- 03_hashing/charhash.cpp
+++ 03_hashing/charhash.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 
+
 int main()
 {
     int size;
+    cout<<"enter size\n";
     cin>>size;
     char charArr[size];
+    map<char,int> countChar;
     for(int i=0;i<size;i++){
         cin>>charArr[i];
+        countChar[charArr[i]]++;
     }
 
-    //pre-Calculating
-    int hash[26] = {0};
-    for(int i=0;i<size;i++){
-        int index = charArr[i] - 'a';
-        hash[index] +=1;
-    }
     int q;
     cout<<"enter query";
     cin>>q;
@@ -25,9 +23,68 @@ int main()
     {
         char c;
         cin>>c;
-        int index  = c - 'a' ;
-        cout<<hash[index]<<endl;
+        cout<<countChar[c]<<endl;
     }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main()
+// {
+//     int size;
+//     cin>>size;
+//     char charArr[size];
+//     for(int i=0;i<size;i++){
+//         cin>>charArr[i];
+//     }
+
+//     //pre-Calculating
+//     int hash[26] = {0};
+//     for(int i=0;i<size;i++){
+//         int index = charArr[i] - 'a';
+//         hash[index] +=1;
+//     }
+//     int q;
+//     cout<<"enter query";
+//     cin>>q;
+//     cout<<"enter elements";
+//     while(q--)
+//     {
+//         char c;
+//         cin>>c;
+//         int index  = c - 'a' ;
+//         cout<<hash[index]<<endl;
+//     }
+
+//     return 0;
+// }

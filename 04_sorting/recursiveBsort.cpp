e4486b1533ae0a6e3b7fdec75601a8133ae9bfb7
GURUGASTGAR <gurugastgar@gmail.com>
2025-03-27 00:05:51 +0530

    inseriton sort recursion

--- 04_sorting/recursiveBsort.cpp
+++ 04_sorting/recursiveBsort.cpp
@@ -6,6 +6,19 @@ void swap(int& a,int &b)
     int temp = a;
     a=b;b=temp;
 }
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        swap(arr[j-1],arr[j]);
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
 void recBubSort(int * arr,int size){
 
     if(size<=1) return;
@@ -29,7 +42,8 @@ void recBubSort(int * arr,int size){
 int main()
 {
     int arr[] = {43,23,24,52,5,1};
-    recBubSort(arr,6);
+    //recBubSort(arr,6);
+    insertion_sort(arr,0,6);
     for(int i=0;i<6;i++){
         cout<<arr[i]<<" ";
     }

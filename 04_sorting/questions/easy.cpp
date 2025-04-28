5fe03a28855e9e2c3ead2d62206dab7614028dcb
GURUGASTGAR <gurugastgar@gmail.com>
2025-04-28 23:14:20 +0530

    consicative one

--- 04_sorting/questions/easy.cpp
+++ 04_sorting/questions/easy.cpp
@@ -112,6 +112,23 @@ class Solution {
 };
 
 
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count =0;
+        int maxCount = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            maxCount = max(maxCount,count);
+        }
+
+        return maxCount;
+}
+
+
 int main(){ 
     vector<int> a = {0,1,1,3,12};
     Solution s;

b3b1ef14600e14ee5c202bc60f222f0233c5fc05
GURUGASTGAR <gurugastgar@gmail.com>
2025-04-30 01:52:24 +0530

    minor changes

--- 04_sorting/questions/java/queston.java
+++ 04_sorting/questions/java/queston.java
@@ -43,13 +43,34 @@ public class Question1 {
 		}
 		return maxLength;
 	}
+
+	public static int longestSubarrayWithSum(int[] nums, int k) {
+		int i = 0;
+		int sum = 0;
+		int maxLength = 0;
+		int count =0;
+		for(int j =0;j<nums.length;j++) {
+			sum = sum + nums[j];
+			count++;
+			while(sum> k && i<=j) {
+				sum -= nums[i];
+				i++;
+				count--;
+			}
+			
+			if(sum == k) {
+				maxLength = Math.max(maxLength, j-i+1);
+			}
+		}
+		return maxLength;
+	}
 	
 	
 	public static void main(String[] args) {
 
-		int arr[] = {0,1,2,3,5};
+		int arr[] = {-1, 1, 1};
 		
-		System.out.println(longestSubarrayWithSum(arr, 5));
+		System.out.println(longestSubarrayWithSum(arr, 1));
 		
 		
 		

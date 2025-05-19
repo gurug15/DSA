13b3b262613749d609a93bc84773e23e606f2176
GURUGASTGAR <gurugastgar@gmail.com>
2025-05-20 05:07:55 +0530

    checking new things

---
+++ Java/DSA_MEDIUMQuestions/src/com/medium/quesitons/Xorsubarray.java
@@ -0,0 +1,17 @@
+package com.medium.quesitons;
+
+public class Xorsubarray {
+	 
+	public static int subarraysWithXorK(int[] nums, int k) {
+	      
+		
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(5^6^7^8^9);
+
+	}
+
+}

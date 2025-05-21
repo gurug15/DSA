591fe478f4f9fd23224c41a35443e4580a0a7515
GURUGASTGAR <gurugastgar@gmail.com>
2025-05-21 21:09:58 +0530

    learend mergeintervals

---
+++ 05_arrays/56_MergeIntervals.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        vector<vector<int>> merge;
+
+        sort(arr.begin(),arr.end());
+        for(int i =0 ; i <arr.size();i++){
+            if(merge.empty() || arr[i][0]>merge.back()[1]){
+                merge.push_back(arr[i]);
+            }else{
+                merge.back()[1] = max(arr[i][1], merge.back()[1]);
+            }
+        }
+        return merge;
+    }
+
+
+int main(){
+    vector<vector<int>> arr = {{1,3},{2,6},{8,10},{15,18}};
+    for (auto ele : merge(arr)) { 
+    cout << ele[0] << " " << ele[1] << endl;
+    }
+
+    
+    
+    return 0;
+}
